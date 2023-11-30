409f1caa4241b95711e7075aee42292b47af7448
Sofia Mamchur <sofia.n.mamchur@gmail.com>
2023-11-29 22:33:04 -0500

    Polymorphism

---
+++ src/day40_exception/throws_keyword/UsingPerson.java
@@ -0,0 +1,9 @@
+package day40_exception.throws_keyword;
+
+public class UsingPerson {
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread.sleep(2000);
+
+    }
+}
